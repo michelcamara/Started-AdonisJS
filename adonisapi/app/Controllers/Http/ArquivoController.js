159705a69c24cc2b9e4e853ed43044971995cb19
Michel Câmara <michelvictor16@gmail.com>
2019-12-09 17:16:43 -0400

    API com AdonisJS
    Aprendendo a fazer um CRUD com Adonis, relacionamentos e upload de arquivos.

---
+++ adonisapi/app/Controllers/Http/ArquivoController.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Arquivo = use('App/Models/Arquivo');
+const Tarefa = use('App/Models/Tarefa');
+
+const Helpers = use('Helpers');
+
+class ArquivoController {
+  async create({params, request, response}){
+    try {
+
+      const tarefa = await Tarefa.findOrFail(params.id);
+
+      const arquivos = request.file('file',{
+        size : '1mb'
+      });
+
+      await arquivos.moveAll(Helpers.tmpPath('arquivos'), file=>({
+        name: `${Date.now()}-${file.clientName}`
+      }));
+
+      if(!arquivos.movedAll()){
+        return arquivos.errors();
+      }
+
+      // await Promise.all(
+      //   arquivos.movedList().map(item => Arquivo.create({tarefa_id: tarefa.id, caminho:item.fileName}))
+
+      // );
+
+      //Fazendo a tratativa que todo arquivo pertence a uma tarefa
+      await Promise.all(
+        arquivos
+        .movedList()
+        .map(item => tarefa.arquivos().create({caminho: item.fileName}))
+
+      );
+
+
+      return response.status(200).send({message: 'Arquivos inseridos com sucesso.'});
+
+    } catch (err) {
+      return response.status(500).send({erro: 'Ocorreu um erro no upload.'});
+    }
+  }
+}
+
+module.exports = ArquivoController
